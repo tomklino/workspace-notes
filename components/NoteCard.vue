2a92399d4854ccfdb46e2b3ac64f1d2bd5e597e2
Tom Klinovsky <tom.klino@gmail.com>
2023-10-05 15:12:03 +0300

    more TODO comments

--- components/NoteCard.vue
+++ components/NoteCard.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container p-4 mx-0 my-5 shadow-md rounded-lg bg-white max-w-3xl">
+        <!--TODO display metadata on the note (date, tags, etc.)-->
         <div class="prose" v-html="$mdRenderer(data)"></div>
     </div>
 </template>
--- plugins/markdownit.ts
+++ plugins/markdownit.ts
@@ -4,6 +4,7 @@ export default defineNuxtPlugin(() => {
     const renderer = md()
     return {
         provide: {
+            // TODO fix: Doesn't render checkboxes correctly
             mdRenderer: (content: any): string => {
                 if(typeof content !== 'string')
                     return ""
