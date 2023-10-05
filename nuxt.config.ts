e6649bf33b1bab7b5da3938837db6d2fb6a145cd
Tom Klinovsky <tom.klino@gmail.com>
2023-10-05 13:52:13 +0300

    reading notes config from env var

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     //TODO read from external source (env var, external conf file)
-    dataDir: '/home/tomklino/notes/september.d/workspaces-2023-09-12/'
+    dataDir: process.env['DATA_DIR']
   },
   devtools: { enabled: true },
   modules: [
