7ec0712514f6c1a6a21b582c68f96b8584fe50f3
Tom Klinovsky <tom.klino@gmail.com>
2023-10-05 15:10:06 +0300

    reload template for next action items

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,8 @@
             <nav class="container mx-auto p-4 flex justify-between">
                 <NuxtLink class="font-bold" to="/">Workspace Notes</NuxtLink>
                 <ul class="flex gap-4">
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li class="btn">Products</li>
+                    <!--TODO add input field to take the number of days to look back to-->
+                    <li class="btn">Reload</li>
                 </ul>
             </nav>
         </header>
