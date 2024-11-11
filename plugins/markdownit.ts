bb285bb3e33476baea583eba58f6be7008432807
Tom Klinovsky <tom.klino@gmail.com>
2024-11-11 12:33:02 +0200

    render checkboxes

--- plugins/markdownit.ts
+++ plugins/markdownit.ts
@@ -1,14 +1,18 @@
-import md from "markdown-it"
+import MarkdownIt from "markdown-it"
+import { tasklist } from "@mdit/plugin-tasklist"
 
 export default defineNuxtPlugin(() => {
-    const renderer = md()
+    const md = MarkdownIt()
+        // @ts-ignore - the plugin works but the types are not updated to the
+        // current version of MarkDownIt
+        .use(tasklist, {})
     return {
         provide: {
             // TODO fix: Doesn't render checkboxes correctly
             mdRenderer: (content: any): string => {
                 if(typeof content !== 'string')
                     return ""
-                return renderer.render(content)
+                return md.render(content)
             }
         }
     }
